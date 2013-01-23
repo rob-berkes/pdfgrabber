8b9c083e48b5e34662ddcd009ac43d69e4cde6fb
Rob Berkes <rob.berkes@gmail.com>
2013-01-22 20:47:35 -0600

    trying to get results above 100 working

--- docs.py
+++ docs.py
@@ -16,30 +16,37 @@ def formatTitle(grabTitle):
 	return newTitle
 
 query=urllib.urlencode({'q':'filetype:doc'})
-url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % (query)
+#url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % (query)
+url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=filetype:doc'
 
-num_queries=25*4 
+num_queries=25*4*8 
 count=0
-for start in range(0,100,4):
+STARTAT=240
+for start in range(STARTAT+0,STARTAT+100*3,4):
 	print start
-	request_url=url+'{0}&start='+str(start)
+	request_url=url+'&start='+str(start)
 	print request_url
 	search_results=urllib.urlopen(request_url)
-	json=simplejson.loads(search_results.read())
-	results=json['responseData']['results']
-	for i in results:
+	try:
+		json=simplejson.loads(search_results.read())
+		results=json['responseData']['results']
+	except TypeError:
+		pass
 		try:
-			#print i
-			HASH=hashlib.sha1(i['url']).hexdigest()
-			if db.grabs.find_one({'_id':HASH}):
-				print 'Already dld!\n\n\n\n'
-#			elif i['fileFormat']=="Microsoft Word":
-			else:
-				db.grabs.insert({'_id':HASH})
-				print str(i['url']+"\n\n\n\n")
-               			os.system("wget --no-check-certificate "+str(i['url'])+"\n\n\n\n")
+			for i in results:
+				#print i
+				HASH=hashlib.sha1(i['url']).hexdigest()
+				if db.grabs.find_one({'_id':HASH}):
+					print 'Already dld!\n\n\n\n'
+	#			elif i['fileFormat']=="Microsoft Word":
+				else:
+					db.grabs.insert({'_id':HASH})
+					print str(i['url']+"\n\n\n\n")
+	               			os.system("wget --no-check-certificate "+str(i['url'])+"\n\n\n\n")
 		except KeyError:
 			print "Exception error in results\n\n\n\n"
+		except NameError:
+			pass
 	os.system("mv *.doc  doxc/")	
 #			OFILE=open(str(count)+'.pdf','wb')
 #			OFILE.write(search_results.read())
