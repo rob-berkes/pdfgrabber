91df837b0e1494e4ac237cf9ac4f86fd96eb349d
Rob Berkes <rob.berkes@gmail.com>
2013-01-22 20:34:47 -0600

    continuing coding...

--- docs.py
+++ docs.py
@@ -18,32 +18,35 @@ def formatTitle(grabTitle):
 query=urllib.urlencode({'q':'filetype:doc'})
 url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % (query)
 
-num_queries=80*4 
+num_queries=25*4 
 count=0
-for start in range(200,num_queries,6):
-	request_url='{0}&start={1}'.format(url,start)
+for start in range(0,100,4):
+	print start
+	request_url=url+'{0}&start='+str(start)
 	print request_url
 	search_results=urllib.urlopen(request_url)
 	json=simplejson.loads(search_results.read())
-	try:
-		results=json['responseData']['results']
-		for i in results:
-			print i
+	results=json['responseData']['results']
+	for i in results:
+		try:
+			#print i
 			HASH=hashlib.sha1(i['url']).hexdigest()
 			if db.grabs.find_one({'_id':HASH}):
-				print 'Already dld!'
-			elif i['fileFormat']=="Microsoft Word":
+				print 'Already dld!\n\n\n\n'
+#			elif i['fileFormat']=="Microsoft Word":
+			else:
 				db.grabs.insert({'_id':HASH})
-				print str(i['url'])
-                os.system("wget --no-check-certificate "+str(i['url']))
-	except TypeError, KeyError:
-		pass
+				print str(i['url']+"\n\n\n\n")
+               			os.system("wget --no-check-certificate "+str(i['url'])+"\n\n\n\n")
+		except KeyError:
+			print "Exception error in results\n\n\n\n"
 	os.system("mv *.doc  doxc/")	
 #			OFILE=open(str(count)+'.pdf','wb')
 #			OFILE.write(search_results.read())
 #			OFILE.close()
 #		newTitle=formatTitle(i['title'])
 #		print newTitle+' - '+i['url']
+
 
 
 
--- pdf.py
+++ pdf.py
@@ -6,6 +6,7 @@ import hashlib
 import re
 import os
 import random
+import time
 NUMRANGE=800000
 conn=Connection()
 db=conn.pdfgrabber
@@ -21,8 +22,9 @@ url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % (query)
 num_queries=10*4 
 count=0
 STARTNUM=random.randint(1,NUMRANGE)
-print "starting seed: "+str(STARTNUM)
-for start in range(STARTNUM,STARTNUM+num_queries,4):
+#print "starting seed: "+str(STARTNUM)
+#for start in range(STARTNUM,STARTNUM+num_queries,4):
+for start in range(0,num_queries,4):
 	request_url='{0}&start={1}'.format(url,start)
 	print request_url
 	search_results=urllib.urlopen(request_url)
@@ -40,7 +42,10 @@ for start in range(STARTNUM,STARTNUM+num_queries,4):
                 os.system("wget --no-check-certificate "+str(i['url']))
 	except TypeError, KeyError:
 		pass
-	os.system("mv *.pdf PDFs/")	
+
+	TIMESTR=time.strftime("%H%M%m%Y")
+	os.system("mkdir -p PDFs/"+str(TIMESTR))
+	os.system("mv *.pdf PDFs/"+str(TIMESTR)+"/")	
 #			OFILE=open(str(count)+'.pdf','wb')
 #			OFILE.write(search_results.read())
 #			OFILE.close()
