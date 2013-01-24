e878547eca9a158ac9941a6c5e265fbaf6122661
Rob Berkes <rob.berkes@gmail.com>
2013-01-24 09:46:07 -0600

    got exact url encoding right for search and dl

--- pdf.py
+++ pdf.py
@@ -8,6 +8,7 @@ import os
 import random
 import time
 NUMRANGE=800000
+DICTWORDS=479820
 conn=Connection()
 db=conn.pdfgrabber
 
@@ -16,16 +17,19 @@ def formatTitle(grabTitle):
 	newTitle=re.sub('<,/,\,>',' ',grabTitle)
 	return newTitle
 
-query=urllib.urlencode({'q':'filetype:pdf'})
-url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % (query)
 
-num_queries=10*4 
+num_queries=5*4 
 count=0
 STARTNUM=random.randint(1,NUMRANGE)
 #print "starting seed: "+str(STARTNUM)
 #for start in range(STARTNUM,STARTNUM+num_queries,4):
+lines=[line.strip() for line in open('/usr/share/dict/words')]
+DWORD=random.randint(0,DICTWORDS)
+query=urllib.urlencode({'q':'filetype:pdf'})
+url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' % (query)
+url+='%20'+str(lines[DWORD])
 for start in range(0,num_queries,4):
-	TIMESTR=time.strftime("%H%M%m%Y")
+	TIMESTR=time.strftime("%H%m%d%Y")
 	request_url='{0}&start={1}'.format(url,start)
 	print request_url
 	search_results=urllib.urlopen(request_url)
